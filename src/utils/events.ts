3347745a72be7b541610a64f75fe508f2189e03d
Aditya Baradwaj <adityabaradwaj97@gmail.com>
2022-01-04 11:59:49 -0800

    Fixing npm build

--- src/utils/events.ts
+++ src/utils/events.ts
@@ -1,7 +1,7 @@
-import { Coder, Idl } from "@project-serum/anchor";
+import { Coder, Idl, Event } from "@project-serum/anchor";
 
 // t is a base64 encoded string
-export function decode(t: string, idl: Idl) {
+export function decode(t: string, idl: Idl): Event | null {
   const coder = new Coder(idl);
   const event = coder.events.decode(t);
   return event;
