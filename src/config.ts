3e4df96b40d22ca414080f720b48fd397aec0eec
Mona Lisa <mona.lisa@behind.you>
2022-01-24 15:46:46 -0800

    Changed config keys

--- src/config.ts
+++ src/config.ts
@@ -14,21 +14,18 @@ export const USDC_DECIMALS = 6;
 
 // == 01 CONSTANTS ==
 export const ZO_STATE_KEY = new PublicKey(
-  "HAdeMzG1ZuzhWnt26iyggLhYUen3YosXiD5sgDXJoNDY",
+  "KwcWW7WvgSXLJcyjKZJBHLbfriErggzYHpjS9qjVD5F",
 );
 
 // == 01 PROGRAM IDS ==
 export const ZERO_ONE_PROGRAM_ID = new PublicKey(
-  "DuSPvazsfthvWRuJ8TUs984VXCeUfJ1qbzd8NwkRLEpd",
+  "Zo1ThtSHMh9tZGECwBDL81WJRL6s3QTHf733Tyko7KQ",
 );
 export const ZO_DEX_PROGRAM_ID = new PublicKey(
-  "CX8xiCu9uBrLX5v3DSeHX5SEvGT36PSExES2LmzVcyJd",
+  "ZDxUi178LkcuwdxcEqsSo2E7KATH99LAAXN5LcSVMBC",
 );
 export const SERUM_SPOT_PROGRAM_ID = new PublicKey(
   "DESVgJVGajEgKGXhb6XmqDHGz3VjdgP7rEVESBgxmroY",
-);
-export const SERUM_SWAP_PROGRAM_ID = new PublicKey(
-  "BiUikS42eRsdmkGBu3qXDy8Tu4cuWYYnuFVZzKzjVEET",
 );
 
 // == SOLANA PROGRAM IDS ==
@@ -47,5 +44,6 @@ export const RENT_PROGRAM_ID = new PublicKey(
 export const WRAPPED_SOL_MINT = new PublicKey(
   "So11111111111111111111111111111111111111112",
 );
+
 // == CLUSTERS ==
 export const DEV_ENDPOINT = "https://api.devnet.solana.com";
